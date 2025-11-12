e3786a5c011f3d682bc0961bd293eac215c68dbc
Mohkadem <s202245820@kfupm.edu.sa>
2025-11-12 09:08:43 +0300

    Task 3 completed ( by mistake I wrote it is done in the previous commit)

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -136,5 +136,13 @@ app.get("/profile/:first/:last", (req, res) => {
 });
 
 // Route param middleware example: /users/42
-
+app.param("userId", (req, res, next, userId) => {
+  const userIdNum = Number(userId);
+  if (userIdNum <= 0) {
+    res.json({ ok: false, error: "userId must be positive number" });
+    return;
+  }
+  req.userIdNum = userIdNum;
+  next();
+});
 // Route params: /users/:userId route
